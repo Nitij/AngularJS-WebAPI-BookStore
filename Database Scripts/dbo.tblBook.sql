d417db8dd69ad7234676abdb074251a7023ff409
Nitij <nitij.jp@gmail.com>
2014-03-09 13:01:44 +0530

    Adding SQL Scripts

---
+++ Database Scripts/dbo.tblBook.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblBook] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Name]            NVARCHAR (100) NOT NULL,
+    [AuthorName]      NVARCHAR (50)  NOT NULL,
+    [PublisherName]   NVARCHAR (50)  NULL,
+    [Price]           INT            NOT NULL,
+    [Discount]        REAL           NOT NULL,
+    [Rating]          INT            NOT NULL,
+    [Language]        NVARCHAR (50)  NOT NULL,
+    [PublicationYear] INT            NULL,
+    [ISBN13]          NVARCHAR (50)  NULL,
+    [ISBN10]          NVARCHAR (50)  NULL,
+    [Category]        NVARCHAR (50)  NOT NULL,
+    [Image]           NVARCHAR (200) NOT NULL,
+    [Details]         NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
